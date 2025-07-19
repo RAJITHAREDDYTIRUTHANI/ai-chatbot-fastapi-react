5a1e5682c1b46f3140d8c953b0dc8d24ce646a6d
RAJITHAREDDYTIRUTHANI <156734896+RAJITHAREDDYTIRUTHANI@users.noreply.github.com>
2025-07-19 12:37:41 -0700

    Connected frontend to live Render backend

--- simplechatbot-frontend/src/App.js
+++ simplechatbot-frontend/src/App.js
@@ -14,7 +14,8 @@ function App() {
     setLoading(true);
 
     try {
-      const response = await fetch('http://127.0.0.1:8000/chat', {
+      const response = await fetch('https://ai-chatbot-k3wk.onrender.com/chat', {
+        //http://127.0.0.1:8000/chat
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
